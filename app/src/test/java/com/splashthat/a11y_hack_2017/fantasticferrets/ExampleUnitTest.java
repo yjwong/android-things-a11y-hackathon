50b64f1557dac3b0c947d4515d71ecc34a102059
Wong Yong Jie <yjwong92@gmail.com>
2017-06-03 10:26:52 -0400

    Initial commit.

---
+++ app/src/test/java/com/splashthat/a11y_hack_2017/fantasticferrets/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.splashthat.a11y_hack_2017.fantasticferrets;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
